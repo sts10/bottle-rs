5ceb388ab2383516ed308f2649546da3b4bcc4cb
Sam Schlinkert <sschlinkert@gmail.com>
2022-01-07 18:42:53 -0500

    encrypts file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fs;
 use std::io::{Read, Write};
 use std::iter;
+
 fn main() {
     const KEY_FILE: &str = "test.txt";
     let identify_file_entry = &age::IdentityFile::from_file(KEY_FILE.to_string())
@@ -11,7 +13,12 @@ fn main() {
     };
     let pubkey = key.to_public();
 
-    let plaintext = b"Hello world!";
+    // let plaintext = b"Hello world!";
+    // need to read plain.txt into bytes here
+    let file_name_to_encrypt = "plain.txt";
+    // let file = File::open(file_name_to_encrypt).expect("file not found!");
+    // let reader = BufReader::new(file);
+    let file_to_encrypt = &fs::read(file_name_to_encrypt).expect("Unable to read file to encrypt");
 
     // Encrypt the plaintext to a ciphertext...
     let encrypted = {
@@ -19,7 +26,8 @@ fn main() {
 
         let mut encrypted = vec![];
         let mut writer = encryptor.wrap_output(&mut encrypted).unwrap();
-        writer.write_all(plaintext).unwrap();
+        // writer.write_all(plaintext).unwrap();
+        writer.write_all(file_to_encrypt).unwrap();
         writer.finish().unwrap();
 
         encrypted
@@ -41,6 +49,9 @@ fn main() {
         decrypted
     };
 
-    assert_eq!(decrypted, plaintext);
-    println!("Hello, world!");
+    // assert_eq!(decrypted, plaintext);
+    assert_eq!(decrypted, *file_to_encrypt);
+    println!("Done!");
 }
+
+// fn read_key_from_file(file_name: &str) -> age::Identity {}
