7d797f5edc3569a9cea7056fd569a42744a5708a
Sam Schlinkert <sschlinkert@gmail.com>
2022-01-07 18:56:33 -0500

    reads key is now sep function

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,7 @@ use std::iter;
 
 fn main() {
     const KEY_FILE: &str = "test.txt";
-    let identify_file_entry = &age::IdentityFile::from_file(KEY_FILE.to_string())
-        .expect("Error reading key from file")
-        .into_identities()[0];
-    let key = match &identify_file_entry {
-        age::IdentityFileEntry::Native(i) => i,
-        _ => unreachable!(),
-    };
+    let key = read_key_from_file(KEY_FILE);
     let pubkey = key.to_public();
 
     // let plaintext = b"Hello world!";
@@ -18,7 +12,7 @@ fn main() {
     let file_name_to_encrypt = "plain.txt";
     // let file = File::open(file_name_to_encrypt).expect("file not found!");
     // let reader = BufReader::new(file);
-    let file_to_encrypt = &fs::read(file_name_to_encrypt).expect("Unable to read file to encrypt");
+    let file_to_encrypt = fs::read(file_name_to_encrypt).expect("Unable to read file to encrypt");
 
     // Encrypt the plaintext to a ciphertext...
     let encrypted = {
@@ -27,7 +21,7 @@ fn main() {
         let mut encrypted = vec![];
         let mut writer = encryptor.wrap_output(&mut encrypted).unwrap();
         // writer.write_all(plaintext).unwrap();
-        writer.write_all(file_to_encrypt).unwrap();
+        writer.write_all(&file_to_encrypt).unwrap();
         writer.finish().unwrap();
 
         encrypted
@@ -42,7 +36,7 @@ fn main() {
 
         let mut decrypted = vec![];
         let mut reader = decryptor
-            .decrypt(iter::once(key as &dyn age::Identity))
+            .decrypt(iter::once(&key as &dyn age::Identity))
             .unwrap();
         reader.read_to_end(&mut decrypted);
 
@@ -50,8 +44,19 @@ fn main() {
     };
 
     // assert_eq!(decrypted, plaintext);
-    assert_eq!(decrypted, *file_to_encrypt);
+    assert_eq!(decrypted, file_to_encrypt);
     println!("Done!");
 }
 
-// fn read_key_from_file(file_name: &str) -> age::Identity {}
+fn read_key_from_file(file_name: &str) -> age::x25519::Identity {
+    let identify_file_entry = age::IdentityFile::from_file(file_name.to_string())
+        .expect("Error reading key from file")
+        .into_identities();
+    // Bummed about this clone(), but can't figure out another way
+    // right now
+    let key = match identify_file_entry[0].clone() {
+        age::IdentityFileEntry::Native(i) => i,
+        _ => unreachable!(),
+    };
+    key
+}
